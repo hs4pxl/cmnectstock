e6e51757aecd397b54ac8c83b3730fe28a0f2172
hs4pxl <hs4pxl@hotmail.com>
2020-08-07 09:26:18 +0700

    First

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from "react";
+import App from "next/app";
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+interface Props {}
+
+function CMApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default CMApp;
---
+++ pages/stock.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from "react";
+import Header from "../components/layouts/header";
+import Menu from "../components/layouts/menu";
+import Layout from "../components/layouts/layout";
+
+interface Props {}
+
+export default function stock({}: Props): ReactElement {
+  return (
+      <Layout>
+        <h1>Stock</h1>
+        <span>1234</span>
+      </Layout>
+  );
+}
